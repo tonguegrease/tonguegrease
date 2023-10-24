8aea5244b5eabf060a8626708a518b61aa925c60
Anthony Klarman <148516349+tonguegrease@users.noreply.github.com>
2023-10-24 13:03:49 -0400

    Delete Lesson 10 - list functions.py

--- Lesson 10 - list functions.py
+++
@@ -1,9 +0,0 @@
-lucky_numbers = [4, 8, 15, 87, 23, 5]
-friends = ["Kevin", "Karen", "Jim", "Jim", "Oscar", "Toby"]
-
-# this is showing the different forms of list functions
-# type "friends." to see the different list functions
-# REMEMBER: "friends" is a variable that the user has created
-
-friends2 = friends.copy()
-print(friends2)
