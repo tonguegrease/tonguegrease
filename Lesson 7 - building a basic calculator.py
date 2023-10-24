3678d0faf633a447c7a3a1fd50a8f759bcc0895a
Anthony Klarman <148516349+tonguegrease@users.noreply.github.com>
2023-10-24 13:05:05 -0400

    Delete Lesson 7 - building a basic calculator.py

--- Lesson 7 - building a basic calculator.py
+++
@@ -1,9 +0,0 @@
-
-
-
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-# The "float" function represents whole numbers whereas the "int" function represents only natural numbers
-result = float(num1) + float(num2)
-
-print(result)
